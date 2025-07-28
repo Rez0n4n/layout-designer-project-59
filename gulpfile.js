8ab73c4ebf546835a30446e479fb8be4173f1af2
Rez0n4n <rez0n4ns@gmail.com>
2025-07-28 18:42:56 +0400

    1 stage

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,26 @@
-import { src, dest, watch, series, parallel } from "gulp";
-import sass from "gulp-sass";
-import * as sassCompiler from "sass";
-import pug from "gulp-pug";
-import svgSprite from "gulp-svg-sprite";
-import concat from "gulp-concat";
-
-// Настройка компилятора SASS
-const sassCompilerInstance = sass(sassCompiler);
+const { src, dest, watch, series, parallel } = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass")(require("sass"));
+const pug = require("gulp-pug");
 
 // Компиляция SCSS в CSS
-function compileSass() {
-  return src("app/scss/**/*.scss")
-    .pipe(
-      sassCompilerInstance({ outputStyle: "compressed" }).on(
-        "error",
-        sass.logError
-      )
-    )
-    .pipe(dest("build/css"));
-}
+const sass2css = () => {
+  return src(["./app/scss/main.scss"])
+    .pipe(sass())
+    .pipe(concat("main.css"))
+    .pipe(dest("./build/styles/"));
+};
 
 // Компиляция Pug в HTML
-function compilePug() {
-  return src("app/pug/**/*.pug")
-    .pipe(pug({ pretty: true }))
-    .pipe(dest("build"));
-}
-
-// Копирование шрифтов
-function copyFonts() {
-  return src("app/fonts/**/*").pipe(dest("build/fonts"));
-}
-
-// Копирование изображений
-function copyImages() {
-  return src("app/img/**/*").pipe(dest("build/img"));
-}
+const pug2html = () => {
+  return src(["./app/**/*.pug"]).pipe(pug()).pipe(dest("./build/"));
+};
 
 // Наблюдение за изменениями
-function watchFiles() {
+const watchFiles = () => {
   watch("app/scss/**/*.scss", compileSass);
   watch("app/pug/**/*.pug", compilePug);
-  watch("app/img/icons/**/*.svg", createSvgSprite);
-  watch("app/js/**/*.js", concatJs);
-  watch("app/fonts/**/*", copyFonts);
-  watch("app/img/**/*", copyImages);
-}
+};
 
-// Основная задача
-export default series(
-  parallel(
-    compileSass,
-    compilePug,
-    createSvgSprite,
-    concatJs,
-    copyFonts,
-    copyImages
-  ),
-  watchFiles
-);
-
-// Задача для сборки без наблюдения
-export const build = series(
-  parallel(
-    compileSass,
-    compilePug,
-    createSvgSprite,
-    concatJs,
-    copyFonts,
-    copyImages
-  )
-);
+exports.default = parallel(sass2css, pug2html);
+exports.watch = series(exports.default, watchFiles);
